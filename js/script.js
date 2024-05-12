df3c35c5cb8e237cfe4aad654b8f24f062b4dbdf
hasa-a-a <hassan_ali_ahmad@outlook.com>
2024-05-12 22:04:27 +0300

    add code to change icon from sun to moon and vice versa

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
+const moonIcon = document.querySelector('.moon');
+const sunIcon = document.querySelector('.sun');
 const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
 if (isDarkMode) {
-    body.classList.add('dark-mode');
-    darkModeToggle.checked = true;
+  body.classList.add('dark-mode');
+  darkModeToggle.checked = true;
+  moonIcon.style.display = 'inline-block';
+  sunIcon.style.display = 'none';
+} else {
+  moonIcon.style.display = 'none';
+  sunIcon.style.display = 'inline-block';
 }
 darkModeToggle.addEventListener('change', () => {
-    if (darkModeToggle.checked) {
-        body.classList.add('dark-mode');
-        localStorage.setItem('darkMode', 'enabled');
-        
-    } else {
-        body.classList.remove('dark-mode');
-        localStorage.setItem('darkMode', 'disabled');
-    }
+  if (darkModeToggle.checked) {
+    body.classList.add('dark-mode');
+    moonIcon.style.display = 'inline-block';
+    sunIcon.style.display = 'none';
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    body.classList.remove('dark-mode');
+    moonIcon.style.display = 'none';
+    sunIcon.style.display = 'inline-block';
+    localStorage.setItem('darkMode', 'disabled');
+  }
 });
